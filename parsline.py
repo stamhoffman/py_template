34007767b63f9e9d78c401e70511b6451af9ca55
stamhoffman <stamhoffman@yandex.ru>
2017-12-25 00:08:02 +0300

    фукнкции

---
+++ parsline.py
@@ -0,0 +1,29 @@
+
+def foo(x, y):
+    return x + y
+
+def foo_1(a, b, c = 2):
+    return a + b + c
+
+def foo_2(n):
+    def foo_3(m):
+        return m + n
+    return foo_3
+
+ret1 = foo_2(100);
+
+print(ret1(100))
+
+def foo_4(n):
+    m = n**2
+
+def foo_5(*args):
+    print(*args)
+
+foo_5(1, 2, "hello")
+foo_5()
+foo_5(1)
+
+foo_6 = lambda x, y: x + y
+
+print("5 + 6 = ",foo_6(5,6))
