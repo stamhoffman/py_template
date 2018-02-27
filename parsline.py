63561443a924b2ca751e65a336ac912c382abd14
stamhoffman <stamhoffman@yandex.ru>
2018-02-27 04:25:06 +0300

    парсер данных из excel в файл на библиотеке openpyxl

--- parsline.py
+++ parsline.py
@@ -1,14 +1,23 @@
-import keyword
-import math
+import openpyxl
 
-def fibb(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    else :
-        return fibb(n - 1 ) + fibb(n - 2 )
+file_out = open('Fab_cable_correction', 'w')
 
-print (fibb( int(input())))
+from  openpyxl  import  load_workbook
+wb = load_workbook('calc.xlsx')
+sheet = wb['Данные для импорта']
+
+popravki = [[0] * 38 for i in range(12)]
+
+
+
+for i in range(12):
+    for j in range(38):
+        row = sheet.cell(row = (i + 1), column = (j + 1)).value
+        popravki[i][j] = row
+
+for i in range(12):
+    for j in range(38):
+        file_out.write(str(popravki[i][j]) + ' ')
+    file_out.write('\n')
+
+file_out.close()
